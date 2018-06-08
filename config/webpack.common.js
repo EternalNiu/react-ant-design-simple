32921651da3105f89f381d666f1a62ea958851de
子墨 <wx-niuqc@ibuscloud.com>
2018-06-08 16:05:20 +0800

    [core] 配置支持less

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -16,6 +16,18 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(less)$/,
+        use: [{
+          loader: 'style-loader',
+        }, {
+          loader: 'css-loader',
+        }, {
+          loader: 'less-loader', options: {
+            javascriptEnabled: true,
+          },
+        }],
+      },
+      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
